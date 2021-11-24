96c6e3b2c21ec08307ee01cd761e92e3deaa6000
Timo Zandbergen <zandbergentimo@gmail.com>
2021-11-24 14:13:51 +0100

    Countdown done

--- Assets/Scripts/Countdown.cs
+++ Assets/Scripts/Countdown.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Countdown : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public float timeleft = 15.0f;
+    public Text countDown;
 
     // Update is called once per frame
     void Update()
     {
+        if (timeleft >= 0)
+        {
+            timeleft -= Time.deltaTime;
+            countDown.text = (timeleft).ToString("0");
+            print(timeleft);
+        }
+
+        if (timeleft < 1 | timeleft == 0)
+        {
+            SceneManager.LoadScene(2);
+        }
+        
         
     }
 }
